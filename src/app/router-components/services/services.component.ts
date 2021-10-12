7241e5c939b97da92c6c9bdfea6caee209821f28
thirtytech <thirtytech@outlook.com>
2021-10-12 23:07:19 +0000

    store side menu refactor

--- src/app/router-components/services/services.component.ts
+++ src/app/router-components/services/services.component.ts
@@ -1,4 +1,8 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, ViewChild } from '@angular/core';
+
+export interface DialogData {
+    label: 'panda' | 'unicorn' | 'lion';
+}
 
 @Component({
     selector: 'services-router',
@@ -26,6 +30,9 @@ export class ServicesComponent implements AfterViewInit{
         var itemWidth = this.initialServicesGridItem.nativeElement.offsetWidth;
         
         this.servicesGridList.nativeElement.style = "padding-left: " + ((itemWidth - this.imageWidth) / 2) + "px;";
+    }
+
+    openDialog() {
     }
 
 }
