02a1452621a29d3a8838350d9a8c6bf82fa3cdea
Gonçalo Valentim Vieira Correia <thirtytech@outlook.com>
2021-12-02 20:54:26 -0100

    added store basic functionality

--- src/app/router-components/store/store.component.ts
+++ src/app/router-components/store/store.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface StoreMenuDataStructureInterface {
+    id: number,
+    label: string,
+    categories: [
+        {
+            id: number,
+            label: string,
+            subCategories: [
+                string
+            ]
+        }
+    ]
+}
 
 @Component({
     selector: 'store-router',
@@ -8,5 +23,30 @@ import { CommonModule } from "@angular/common";
 })
 
 export class StoreComponent{
+
+    private _jsonURL = '../../../assets/store-menu-datastructure.json';
+    
+    menuDataStructure : StoreMenuDataStructureInterface[] = [{id: 0, label:"", categories: [{id: 0, label: "", subCategories: [""]}]}];
+    menuItem : StoreMenuDataStructureInterface = {id: 0, label:"", categories: [{id: 0, label: "", subCategories: [""]}]};
+    categories: [string] = [""];
+
+    constructor(private http: HttpClient) {
+        this.getJSON().subscribe(data => {
+            this.menuDataStructure = data;
+        });
+    }
+
+    private getJSON(): Observable<any> {
+        return this.http.get(this._jsonURL);
+    }
+
+    public onClick_menuItem(index: number) {
+        this.menuItem = this.menuDataStructure[index];
+        this.categories = [""];
+    }
+
+    public onClick_category(index: number) {
+        this.categories = this.menuItem.categories[index].subCategories;
+    }
 
 }
